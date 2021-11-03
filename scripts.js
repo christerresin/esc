8d127d7a1f9ac5304e7df26cc5654a0fd549c6af
Chrikelin <Christer.resin@gmail.com>
2021-11-03 13:54:20 +0100

    add conditional for mobile and desktop menu and exit button on mobileMenu

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
+// Using DOM API to select elements
 const navbar = document.querySelector('.navbar');
+const dropDownMenu = document.querySelector('.dropDownMenu')
  
-const createMenu = function(){
+const createMenu = function(el){
+    let isMobile;
+
+    if (el === navbar) {
+        isMobile = false; 
+    } else {
+        isMobile = true;
+    }
+
     const menuEl = document.createElement('ul');
-    menuEl.classList.add('menu');
+    menuEl.classList.add(isMobile? 'mobileMenu' : 'menu');
     const menuItems = ['Play Online', 'Play on-site', 'The story', 'Contact Us'];
 
+    // Looping over all menu items creating li's, appending ul
     menuItems.forEach((item) => {
         const menuLinkEl = document.createElement('li');
         menuLinkEl.innerText = item;
         menuEl.appendChild(menuLinkEl);
     })
 
-    navbar.append(menuEl)
-
+    el.append(menuEl)
 }
 
-createMenu();
+createMenu(navbar);
+createMenu(dropDownMenu)
